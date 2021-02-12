15d8b17782677d1bc19a71177274c22ca0e45968
Grace229 <59564004+Grace229@users.noreply.github.com>
2021-02-12 18:36:36 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,42 +9,6 @@ const { hashPassword, comparePassword } = Encrypt;
 const { createToken } = Token;
 
 export default class Auth {
-  static async register(request, response, next) {
-    try {
-      const result = request.body;
-      // const emailExist = await User.findOne({ email: result.email });
-      // const usernameExist = await User.findOne({ username: result.username });
-      // const role = await Roles.findOne({ name: "user" });
-      // if (emailExist) {
-      //   throw createError.BadRequest(`${result.email} is already in use`);
-      // }
-      // if (usernameExist) {
-      //   throw createError.BadRequest(`${result.username} is already in use`);
-      // }
-      // if (!role) {
-      //   throw createError.BadRequest(`An Error occured please contact support`);
-      // }
-      result.password = hashPassword(result.password);
-      // result.role = role._id;
-      let user = new User(result);
-      let data = await user.save();
-      // let token = createToken(data._id);
-      // let link = `${clientUrl}confirm-account/${token}`;
-      // const options = {
-      //   mail: result.email,
-      //   subject: "Welcome to Good Deeds!, confirm your email",
-      //   email: "../email/welcome.html",
-      //   variables: { name: result.username, link: link },
-      // };
-      // await Mail(options);
-      return response
-        .status(200)
-        .send(data);
-    } catch (error) {
-      if (error.isJoi === true) error.status = 422;
-      next(error);
-    }
-  }
   static async login(request, response, next) {
     try {
       let emailVerify = { email: request.body.email };
@@ -64,7 +28,10 @@ export default class Auth {
       );
       if (passwordMatch) {
         const token = createToken(user);
-        return response.status(200).send(token);
+        return response.status(200).json({
+        user: user,
+          token: token
+        });
       } else {
         throw createError.BadRequest("Email/password not valid");
       }
